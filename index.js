04f5af194882fb45eda34236bc7c722582612307
nawagest <nathanaeldt25@gmail.com>
2022-07-24 12:24:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+const dotenv = require('dotenv').config();
+const ejs = require('ejs');
+const port = 3002;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/', (req, res) => {
+    const apiKey = process.env.APPID;
+    const { cityName, units: u} = req.body;
+    const q = cityName;
+    
+    // const units = req.body.units === 'F' || req.body.units === 'f' ? 'imperial' : 'metric';
+
+    let units = 'imperial'; // Default
+    let deg = '';
+    if((u === 'F' || u === 'f' || u === 'Fahrenheit' || u === 'fahrenheit')) {
+        units = 'imperial';
+        deg = '℉'
+    } else if((u === 'C' || u === 'c' || u === 'Celsius' || u === 'celsius')) {
+        units = 'metric';
+        deg = '℃';
+    }
+
+    const symbol = units === 'imperial' ? '℉' : '℃';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${apiKey}&units=${units}`;
+    let iconFile = '';
+
+    https.get(url, (response) => {
+
+        const status = response.statusCode;
+
+        response.on('data', (data) => {
+
+            if(status === 200) {
+
+                // Destructure this further (MAKE IT LOOK NICER)
+                const jsonData = JSON.parse(data);
+                const { weather, name, sys } = jsonData;
+                const { country } = sys;
+                const { main } = jsonData;
+                const { temp, feels_like, temp_min, temp_max, humidity } = main;
+                const [ info ] = weather;
+                const { main: sky, description: desc, icon } = info;
+                iconFile = icon;
+                // console.log(info);
+
+                res.render('weather', { temp, feels_like, temp_min, temp_max, humidity, sky, desc, icon, country, name, deg });
+            } else {
+                const { cod, message } = JSON.parse(data);
+                res.render('error', { cod, message });
+            }
+            // res.render('index', { jsonData });
+        });
+    });
+});
+
+app.post('/redirect', (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// IT WOOORKRKKRS IM SO HAPPY RN :D
+// alert('Test');
+
+// const inputs = document.querySelectorAll('input');
