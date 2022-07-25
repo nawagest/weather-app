9a8d536119dbd1ca8aa5e8e6d5a6b13e3ee4018b
nawagest <nathanaeldt25@gmail.com>
2022-07-25 15:48:51 -0400

    fixed all errors

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const https = require('https');
 const dotenv = require('dotenv').config();
 const ejs = require('ejs');
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -71,6 +71,6 @@ app.post('/coords', (req, res) => {
     console.log(req);
 });
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
