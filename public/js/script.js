611e833adad20ce4df1fda51a94983a060362ed9
nawagest <nathanaeldt25@gmail.com>
2022-07-25 13:57:54 -0400

    add back geolocation button and code

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+const geoBtn = document.querySelector('.geo-btn');
+
+geoBtn.addEventListener('click', () => {
+
+    const geolocation = navigator.geolocation;
+
+    geolocation.getCurrentPosition((coords) => {
+
+        const { coords: { latitude: lat, longitude: lng } } = coords;
+        const data = JSON.stringify({ lat, lng });
+        console.log(coords)
+        console.log(data);
+
+        fetch('/coords', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                data
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    });
+});
