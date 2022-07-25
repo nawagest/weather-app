741446398d09c2fbfc2646366895d590ac6503bd
nawagest <nathanaeldt25@gmail.com>
2022-07-25 10:34:04 -0400

    make code neat

--- index.js
+++ index.js
@@ -25,18 +25,14 @@ app.post('/', (req, res) => {
     // const units = req.body.units === 'F' || req.body.units === 'f' ? 'imperial' : 'metric';
 
     let units = 'imperial'; // Default
-    let deg = '';
     if((u === 'F' || u === 'f' || u === 'Fahrenheit' || u === 'fahrenheit')) {
         units = 'imperial';
-        deg = '℉'
     } else if((u === 'C' || u === 'c' || u === 'Celsius' || u === 'celsius')) {
         units = 'metric';
-        deg = '℃';
     }
 
     const symbol = units === 'imperial' ? '℉' : '℃';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${apiKey}&units=${units}`;
-    let iconFile = '';
 
     https.get(url, (response) => {
 
@@ -57,7 +53,7 @@ app.post('/', (req, res) => {
                 iconFile = icon;
                 // console.log(info);
 
-                res.render('weather', { temp, feels_like, temp_min, temp_max, humidity, sky, desc, icon, country, name, deg });
+                res.render('weather', { temp, feels_like, temp_min, temp_max, humidity, sky, desc, icon, country, name, symbol });
             } else {
                 const { cod, message } = JSON.parse(data);
                 res.render('error', { cod, message });
--- public/js/script.js
+++ public/js/script.js
@@ -5,14 +5,14 @@ geoBtn.addEventListener('click', () => {
     const geolocation = navigator.geolocation;
 
     geolocation.getCurrentPosition((coords) => {
-        
-        const { coords: {latitude:lat, longitude:lng} } = coords;
-        const data = JSON.stringify({lat, lng });
+
+        const { coords: { latitude: lat, longitude: lng } } = coords;
+        const data = JSON.stringify({ lat, lng });
         console.log(coords)
         console.log(data);
 
-        fetch('/coords', { 
-            method: 'POST', 
+        fetch('/coords', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -20,7 +20,7 @@ geoBtn.addEventListener('click', () => {
                 data
             }
         })
-        .then(res => res.json())
-        .then(data => console.log(data));
+            .then(res => res.json())
+            .then(data => console.log(data));
     });
 });
